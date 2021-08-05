c3f66de4dd105b6e0e8934d5aed8c9ac6764e71f
dudusamidi <nanda.spirit@gmail.com>
2021-08-05 21:16:31 +0700

    commit

---
+++ days4.js
@@ -0,0 +1,57 @@
+
+// var nilai = prompt('masukan angka :');
+// if(nilai % 2 ==0){
+//     document.write(nilai + ' merupakan bilangan genap')
+// }else if(nilai % 2 ==1){
+//     document.write(nilai + ' merupakan bilangan ganjil')
+// }else{
+//     document.write(nilai + ' bukan merupakan bilangan')
+// } 
+
+// var nilaiKepuasan = parseInt(prompt("masukan nilai kepuasan kamu",'0-100'));
+
+// switch(nilaiKepuasan){
+//     case (nilaiKepuasan>=90):
+//         document.write('Nilai kamu adalah A')
+//         break 
+//     case nilaiKepuasan>=80:
+//         document.write('Nilai kamu adalah B')
+//         break
+//     case nilaiKepuasan>=70 :
+//         document.write('Nilai kamu adalah C')
+//         break
+//     case nilaiKepuasan>=60 :
+//         document.write('Nilai kamu adalah D')
+//         break      
+
+// }
+
+
+
+
+//JAWABAN SOAL B
+
+var nama = prompt('masukan nama barang :');
+var jumlah = prompt('masukkan jumlah barang')
+var harga = prompt('masukkan harga barang')
+var diskon = prompt('masukkan diskon barang')
+
+
+switch(nama,jumlah,harga,diskon){
+    case (nama = 'monitor'):
+        document.write('Nama barang adalah Monitor')
+        document.write('Nama barang adalah Monitor')
+        document.write('Nama barang adalah Monitor')
+        document.write('Nama barang adalah Monitor')
+        break 
+    case (nama = 'monitor'):
+        document.write('Nilai kamu adalah B')
+        break
+    case (nama = 'monitor'):
+        document.write('Nilai kamu adalah C')
+        break
+    case (nama = 'monitor'):
+        document.write('Nilai kamu adalah D')
+        break      
+
+}
